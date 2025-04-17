736220084a01a75e047f0ea1dc5ea412da930fab
abhinavgupta33 <ag9108074@gmail.com>
2025-04-17 06:56:05 +0530

    commit

--- router.js
+++ router.js
@@ -1,7 +1,7 @@
 let express=require("express");
 let router=express.Router();
 let my_auth=require("./my_auth");
-let{const_leaderboard,cont_deleteproduct,cont_mailopen,cont_updateproduct1,cont_updateproduct,cont_confirm_purchase1,cont_confirm_purchase,cont_removeproduct,cont_addtocart,cont_buynow,cont_shopping,cont_viewproduct,cont_addproduct,cont_balance,cont_viewprofile,cont_logout,cont_deposit1,cont_deposit,cont_withdraw1,cont_withdraw,cont_change_pass1,cont_admininfo,cont_update,cont_userupdate,cont_userunblock,cont_userblock,cont_filteruser,cont_loginrout,cont_viewuser,cont_userview,cont_register,cont_adduserdata,cont_login,cont_adduser,cont_registerr,cont_adminprofile,cont_dashboard,cont_otp,cont_changepass}=require("./controller");
+let{cont_leaderboard,cont_deleteproduct,cont_mailopen,cont_updateproduct1,cont_updateproduct,cont_confirm_purchase1,cont_confirm_purchase,cont_removeproduct,cont_addtocart,cont_buynow,cont_shopping,cont_viewproduct,cont_addproduct,cont_balance,cont_viewprofile,cont_logout,cont_deposit1,cont_deposit,cont_withdraw1,cont_withdraw,cont_change_pass1,cont_admininfo,cont_update,cont_userupdate,cont_userunblock,cont_userblock,cont_filteruser,cont_loginrout,cont_viewuser,cont_userview,cont_register,cont_adduserdata,cont_login,cont_adduser,cont_registerr,cont_adminprofile,cont_dashboard,cont_otp,cont_changepass}=require("./controller");
 router.get("/",cont_login);
 router.get("/register",my_auth,cont_register);
 router.post("/loginrout",cont_loginrout);
@@ -42,7 +42,7 @@ router.get("/deleteproduct",my_auth,cont_deleteproduct);
 router.post("/updateproductform",my_auth,cont_updateproduct1);
 router.post("/send-mail",my_auth);
 router.get("/mail-open",my_auth,cont_mailopen);
-rputer.get("/leaderboard",my_auth,const_leaderboard);
+router.get("/leaderboard",my_auth,cont_leaderboard);
 
 
 
